d86d98161046dbb4cdf50e404173834014a83ee6
Algoruu <zlfkcmspdbdp@naver.com>
2024-10-28 21:44:13 +0900

    임시 업데이트

---
+++ std.js
@@ -0,0 +1,14 @@
+// 변수의 데이터 타입 명시
+var stdId = 1111;
+var stdName = 'kim';
+var age = 25;
+var gender = 'female';
+var course = 'Typescript';
+var completed = false;
+function getInfo(id) {
+    return null; //임시로 null 값 부여
+}
+// 함수의 데이터 타입 명시(매개변수, 리턴타입)
+// function Plus(a : number, b : number) : number{
+//     return a + b;
+// }
---
+++ std.ts
@@ -0,0 +1,20 @@
+// 변수의 데이터 타입 명시
+
+let stdId : number = 1111;
+let stdName : string = 'kim';
+let age : number = 25;
+let gender : string = 'female';
+let course : string = 'Typescript';
+let completed : boolean = false;
+
+function getInfo(id : number) : object {
+    return null; //임시로 null 값 부여
+
+}
+
+
+
+// 함수의 데이터 타입 명시(매개변수, 리턴타입)
+// function Plus(a : number, b : number) : number{
+//     return a + b;
+// }
