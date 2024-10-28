1167775329a45e1b79557a8b076e5878a0499d61
Algoruu <zlfkcmspdbdp@naver.com>
2024-10-28 22:20:20 +0900

    std.ts업데이트

--- std.js
+++ std.js
@@ -5,7 +5,33 @@ var age = 25;
 var gender = 'female';
 var course = 'Typescript';
 var completed = false;
+;
+function getInfo(id) {
+    return {
+        stdId: id,
+        stdName: 'kim',
+        age: 25,
+        gender: 'female',
+        course: 'javascript',
+        completed: true
+    };
+}
+;
+var std = ({
+    stdId: 91011,
+    stdName: 'choi',
+    age: 30,
+    gender: 'male',
+    course: 'node.js',
+    completed: true
+}); // 힙메모리에 저장됨
+function setInfo(student) {
+    console.log(student);
+}
+;
+setInfo(std);
+// console.log(getInfo(5678));
 // 함수의 데이터 타입 명시(매개변수, 리턴타입)
-// function Plus(a : number, b : number) : number{
+// function Plus(a : number, b? : number) : number{
 //     return a + b;
 // }
--- std.ts
+++ std.ts
@@ -14,22 +14,41 @@ interface Student{
     gender : string;
     course : string;
     completed : boolean;    
-}
+};
 
-function getInfo(id : number) : {
-    stdId : number;
-    stdName : string;
-    age : number;
-    gender : string;
-    course : string;
-    completed : boolean;
-}
+function getInfo(id : number) : Student
+{
+    return {
+        stdId : id,
+        stdName : 'kim',
+        age : 25,
+        gender : 'female',
+        course : 'javascript',
+        completed : true
+    };
+};
 
+let std = ({
+    stdId : 91011,
+    stdName : 'choi',
+    age : 30,
+    gender : 'male',
+    course : 'node.js',
+    completed : true
+}); // 힙메모리에 저장됨
 
+function setInfo(student : Student) : void{
+    console.log(student);
+
+};
+
+setInfo(std);
+
+// console.log(getInfo(5678));
 
 
 
 // 함수의 데이터 타입 명시(매개변수, 리턴타입)
-// function Plus(a : number, b : number) : number{
+// function Plus(a : number, b? : number) : number{
 //     return a + b;
 // }
